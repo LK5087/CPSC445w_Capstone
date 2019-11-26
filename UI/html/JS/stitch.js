a464bbeceb5b4516d8083bd6ea5872839df9efed
Liam <liam.kempton.16@cnu.edu>
2019-11-26 08:59:17 -0500

    gui work

---
+++ UI/html/JS/stitch.js
@@ -0,0 +1,9 @@
+function moveToNewUserPage(){
+    location.href="new_user.html";
+}
+function moveToForgotPasswordPage(){
+    location.href="forgot_password.html";
+}
+function moveToLoginPage(){
+    location.href="index.html";
+}
